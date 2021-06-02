34aed94798b7284402211758b8a75ffaa89c626d
Dmitry Chiyanov <dimasovskij1999@gmail.com>
2021-06-02 21:09:44 +0300

    не умер, живой в итоге

--- 3.py
+++ 3.py
@@ -1,32 +1,17 @@
-class Error:
-    def __init__(self):
-        self.my_list = []
-
-    @property
-    def my_input(self):
-
-        while True:
-            try:
-                val = int(input('Введите значения и нажимайте Enter. Для выхода пропишите чит-код 1109. '))
-                if val != 1109:
-                    self.my_list.append(val)
-                    print(f'Текущий список - {self.my_list} \n ')
-                else:
-                    print('пока! Итоговый список: ', (self.my_list))
-                    break
-
-            except:
-                print(f"Недопустимое значение - строка и булево")
-                exit_try = input(f'Попробовать еще раз? Y/N')
-
-                if exit_try.lower()== 'y':
-                    print(try_except.my_input)
-                elif exit_try.lower()== 'n':
-                    return f'итоговый список: {self.my_list}'
-                else:
-                    return f'пошел отседова....'
+class NotNumber(ValueError):
+    pass
 
 
+my_list = []
+while True:
+    try:
+        value = input('Введите число в список, q для выхода')
+        if value == 'q':
+            break
+        if not value.isdigit():
+            raise NotNumber(value)
+        my_list.append(int(value))
+    except NotNumber as ex:
+        print('Не число!')
+print(my_list)
 
-try_except = Error()
-print(try_except.my_input)
